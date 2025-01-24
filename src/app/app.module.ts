19ff7148fc0d31287b6e476f3645943d2e9a6db2
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-01-24 07:10:55 +0100

    Display products home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { HomeComponent } from './components/home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  currentYear: number = new Date().getFullYear();
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {
+    
+  }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      next:(data) => {
+        this.products = data;
+      }, error: (error) => {
+        console.log(error);
+      }
+    })
+  }
+
+  trackById(index: number, product: Product): number {
+    return product.id; // Devuelve el id único de cada producto
+  }
 
 }
