5102bffb3c6746c9802a2f0ad54ab38b60a2d4b6
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-01-28 15:44:45 +0100

    Implementing cart funtionalities

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -12,18 +12,36 @@ export class CartService {
   constructor() { }
 
   addItemCart(itemCart: ItemCart) {
-
+    this.items.set(itemCart.productId, itemCart);
   }
 
   deleteItemCart(productId: number) {
-
+    this.items.delete(productId);
+    this.items.forEach(
+      (valor, clave) => {
+        console.log('This is the key and value: ' + clave, valor)
+      }
+    );
   }
 
   totalCart() {
-
+    let totalCart: number = 0;
+    this.items.forEach(
+      (itemValue) => {
+        totalCart += itemValue.price * itemValue.quantity;
+      }
+    );
+    return totalCart;
   }
 
   getListFromMap() {
-    
+    this.itemList.splice(0, this.itemList.length);
+    this.items.forEach(
+      (item) => {
+        this.itemList.push(item)
+      }
+    )
+    return this.itemList;
+
   }
 }
