8ec9a31f7461ba381ecaa3475ed7406d193f2aae
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-01-28 06:56:06 +0100

    implementing category service

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import { Category } from '../interfaces/category';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,19 +15,19 @@ export class CategoryService {
     private http: HttpClient
   ) { }
 
-  getCategoryList() {
-    
+  getCategoryList(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.apiURL);
   }
 
-  createCategory(category: Category) {
-    
+  createCategory(category: Category): Observable<Category> {
+    return this.http.post<Category>(this.apiURL, category)
   }
 
   deleteCategory(id: number) {
-    
+    return this.http.delete<Category>(`${this.apiURL}/${id}`);
   }
 
-  getCategoryById(id: number) {
-    
+  getCategoryById(id: number): Observable<Category> {
+    return this.http.get<Category>(`${this.apiURL}/${id}`);
   }
 }
