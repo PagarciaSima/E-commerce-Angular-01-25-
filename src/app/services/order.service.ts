a798791cbc2355876134c3e0dad201d7e2370da1
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-01-30 19:40:01 +0100

    Implementing order service

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { Order } from '../interfaces/order';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  private apiURL: string = environment.apiURLUsers;
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  createOrder(order: Order): Observable<Order> {
+    return this.httpClient.post<Order>(this.apiURL, order);
+  }
+
+  getTotalPrice(order: Order): number {
+    let totalCounter = 0;
+    for (let orderProduct of order.orderProducts) {
+      totalCounter += orderProduct.quantity * orderProduct.price;
+    }
+    return totalCounter;
+  }
+
+  updateOrder(formData: FormData): Observable<FormData> {
+    return this.httpClient.put<FormData>(`${this.apiURL}/update/state/order`, formData);
+  }
+    
+  getOrderByUserId(userId: number): Observable<Order[]> {
+    return this.httpClient.get<Order[]>(`${this.apiURL}/by-user/${userId}`);
+  }
+
+  getOrderById(id: number): Observable<Order> {
+    return this.httpClient.get<Order>(`${this.apiURL}/${id}`);
+  }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: false,
     apiURLProducts: 'http://localhost:8085/api/v1/admin/products',
     apiURLCategories: 'http://localhost:8085/api/v1/admin/categories',
-    apiURLUsers: 'http://localhost:8085/api/v1/users'
+    apiURLUsers: 'http://localhost:8085/api/v1/users',
+    apiURLOrders: 'http://localhost:8085/api/v1/orders'
   };
